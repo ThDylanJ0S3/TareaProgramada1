2a7bdd8983963198353ed78ef7c2b264e9b4edd8
ThDylanJ0S3 <86325245+ThDylanJ0S3@users.noreply.github.com>
2023-10-02 16:07:57 -0600

    Menú y funciones añadidas
    Se crean los menús para el uso del programa, además, se añaden las funciones de países por continente y de grandes a pequeños de manera totalmente funcional

--- Main.py
+++ Main.py
@@ -4,27 +4,98 @@
 #   Ultimo cambio:                             #
 #   Version:                                   #
 ################################################
-import csv
 
-def crearEstructura():
-    listaPaises = []
-    with open("./data/ArchivoDePaises.csv","r") as archivoP:
-        i = 0
-        lectorCSV = csv.reader(archivoP)
+from funciones import *
 
-        for fila in lectorCSV:          
-            nombre = fila[1]  # countryName
-            codigos = [fila[0],fila[4],fila[5],fila[11],fila[12]]  # countryCode, fipsCode, isoNumeric, isoAlpha3, geonameId
-            currencyCode = fila[2]  # currencyCode
-            population = fila[3]  # population
-            capital = fila[6]  # capital
-            continente = [fila[7],fila[8]]  # continentName, continent
-            areaInSqKm = fila[9]  # areaInSqKm
-            lenguages = fila[10].split(",")
-            filaDatos = [nombre,codigos,currencyCode,population,capital,continente,areaInSqKm,lenguages]
-            listaPaises.append(filaDatos)
-        while i != len(listaPaises):
-            print("Lista",i,listaPaises[i])
-            i += 1
+def menu(listaPaises):
+    print("\n\n******************************************")
+    print("************* Menu Principal *************")
+    print("******************************************\n\n")
+    print("\nDigite 1: Crear estructura")
+    print("\nDigite 2: Generar XML")
+    print("\nDigite 3: Construir HTML")
+    print("\nDigite 4: Salir\n")
+    opcion = int(input("Elija una opción: "))
+    if opcion >=1 and opcion <=4:
+        if opcion == 1:
+            listaPaises = crearEstructura()
+            print("\n\n******************************************")
+            print("************* Archivo Creado *************")
+            print("******************************************\n\n")
+        elif opcion == 2 and listaPaises != []:                
+            crearArchivoXML(listaPaises)
+        elif opcion == 3 and listaPaises != []:
+            print("\n\n******************************************")
+            print("************* Ordenar Países *************")
+            print("******************************************\n\n")
+            print("\nDigite 1: Países por continente")
+            print("\nDigite 2: ¿Cuántos viven?")
+            print("\nDigite 3: De grandes a pequeños")
+            print("\nDigite 4: Zonas Azules")
+            print("\nDigite 5: Países con el mismo idioma")
+            print("\nDigite 6: Pago con la misma moneda")
+            print("\nDigite 7: Códigos de un determinado país")
+            print("\nDigite 8: Hablantes por idioma\n")
+            opcionHTML = int(input("Elija una opción: "))
+            if opcionHTML >=1 and opcion <=8:
+                if opcionHTML == 1:
+                    crearPaisesPorContinente(listaPaises)
+                elif opcionHTML == 2:
+                    crearCuantosViven(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                elif opcionHTML == 3:
+                    crearGrandePeque(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                elif opcionHTML == 4:
+                    crearZonaAzul(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                elif opcionHTML == 5:
+                    crearMismoIdioma(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                elif opcionHTML == 6:
+                    crearMismaMoneda(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                elif opcionHTML == 7:
+                    crearCodigoPais(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                elif opcionHTML == 8:
+                    crearHablantesIdiomas(listaPaises)
+                    print("\n\n******************************************")
+                    print("************* Archivo Creado *************")
+                    print("******************************************\n\n") 
+                else:
+                    print("\n\n******************************************")
+                    print("****** Ingrese una opción del 1 al 8 *****")
+                    print("******************************************\n\n")
+                    return menu(listaPaises)
+        elif opcion == 4:
+            print("\n\n******************************************")
+            print("************ Programa Cerrado ************")
+            print("******************************************\n\n") 
+            return
+        elif 2 <= opcion <= 4 and listaPaises == []:
+            print("\n\n******************************************")
+            print("*** No se ha creado ninguna estructura ***")
+            print("******************************************\n\n")
+            return menu(listaPaises)
+    else:
+        print("\n\n******************************************")
+        print("****** Ingrese una opción del 1 al 4 *****")
+        print("******************************************\n\n")
+    return menu(listaPaises)
 
-crearEstructura()
+# Main
+listaPaises = []
+menu(listaPaises)
